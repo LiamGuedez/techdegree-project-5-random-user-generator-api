d53890651d9da06aaab16fff4e2d20674567390d
LiamGuedez <liamhayes9393@gmail.com>
2018-10-08 17:54:13 -0400

    Display the employees

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 /** Class representing an employee. */
 class Employee
 {
-    constructor(image, name, email, city, mobile, address, birthday)
+    constructor(image, name, email, mobile, address, birthday)
     {
       this.image = image;
       this.name = name;
       this.email = email;
-      this.city = city;
       this.mobile = mobile;
       this.address = address;
       this.birthday = birthday;
@@ -68,19 +67,51 @@ const formatEmployeeData = employeeData =>
       {
         data.picture = data.picture.large;
         data.name = `${titleCase(data.name.first + " " + data.name.last)}`;
-        data.location.city = titleCase(data.location.city);
-        data.address = `${titleCase(data.location.street)}, ${titleCase(data.location.city)}, ${titleCase(data.location.state)}`;
+        data.location = {street: `${titleCase(data.location.street)}`, city: `${titleCase(data.location.city)}`, state: `${titleCase(data.location.state)}`};
         data.cell = formatPhoneNumber(data.cell);
         data.dob.date = `${data.dob.date.substring(5,7)}/${data.dob.date.substring(8,10)}/${data.dob.date.substring(0,4)}`;
         return data;
       })
-      .reduce((employee, data) => new Employee(data.picture, data.name, data.email, data.location.city, data.cell, data.address, data.dob.date),"");
+      .reduce((employee, data) => new Employee(data.picture, data.name, data.email, data.cell, data.location, data.dob.date),"");
       employees.push(data);
     })
 
   return employees;
 }
 
+/**
+ * Appends the employees to the web page.
+ * @param   {Array}   employees - Data to be displayed.
+ * @return  {Nothing right now}    employees - An array of Employee instances.
+ */
+const displayEmployees = employees =>
+{
+  const appendEmployee = employee =>
+  {
+    const employeeCard = document.createElement('DIV');
+    employeeCard.className = 'card';
+    employeeCard.className = 'card';
+    employeeCard.innerHTML =
+    `<div class="card-img-container">
+        <img class="card-img" src="${employee.image}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${employee.name}</h3>
+        <p class="card-text">${employee.email}</p>
+        <p class="card-text cap">${employee.address.city}, ${employee.address.state}</p>
+    </div>`;
+
+    const galleryDiv = document.querySelector('#gallery');
+    galleryDiv.appendChild(employeeCard);
+  }
+
+  employees.forEach(employee => appendEmployee(employee));
+  console.log(employees);
+}
+
 getEmployeeData
   .then(employeeData => formatEmployeeData(employeeData))
-  .then(employees => console.log(employees));
+  .then(employees => displayEmployees(employees));
+
+
+  // console.log(searchContainer, galleryDiv, modalContainer);
